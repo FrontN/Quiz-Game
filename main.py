caffe455aa0d7dd989f12e1b2c2901664c71e669
FrontN <158281796+FrontN@users.noreply.github.com>
2026-04-06 14:18:09 +0200

    feat: add game loop, play-again logic and input validation
    - Implemented a main game loop using 'keep_going' to allow multiple sessions.
    - Added 'get_valid_input' function to handle user choices and reduce errors.

--- main.py
+++ main.py
@@ -11,7 +11,34 @@ def clear_screen():
     On Unix-based systems, this function executes the 'clear' command in the terminal."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def get_valid_input(text, options):
+    """Asks the user for input and checks if it is one of the options provided.
+    If the user's input is not one of the options, it will print "non valid answer. please try again"
+    and ask for the input again."""
+    while True:
+        user_input = input(f"{text} ({options[0]}/{options[-1]}): ").lower()
+        if user_input in options:
+            return user_input
+        else:
+            print("non valid answer. please try again")
+            time.sleep(1.5)
+            clear_screen()
+
 def main():
+    """
+    Starts the quiz game.
+
+    This function creates a list of Question objects from the data in data.py,
+    creates a QuizBrain object with the list of questions, and then
+    starts the quiz game in a loop until the user decides to quit
+    or all questions have been answered.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     question_bank = []
 
     for question in question_data:
@@ -21,12 +48,24 @@ def main():
         question_bank.append(new_questions)
 
     quiz = QuizBrain(question_bank)
-    while quiz.still_has_questions():
-        quiz.next_question()
+    keep_going = True
+    while keep_going:
+        clear_screen()
+        print("Welcome to the quiz game!")
         time.sleep(1.5)
         clear_screen()
-    print("You've completed the quiz")
-    print(f"Your final score was: {quiz.score}/{quiz.question_nb}")
+        while quiz.still_has_questions():
+            quiz.next_question()
+            time.sleep(1.5)
+            clear_screen()
+        print("You've completed the quiz")
+        print(f"Your final score was: {quiz.score}/{quiz.question_nb}")
+        time.sleep(3)
+        clear_screen()
+
+        if get_valid_input("Do you want to play again?", ["yes", "y", "no", "n"]).startswith("n"):
+            keep_going = False
+            print("See You!!")
 
 if __name__ == "__main__":
     main()
